e6e71356d7d6a651138c2b7ffb3110a1f60b65ba
aadridi <aadridi@gmail.com>
2024-05-14 13:34:34 +0100

    Last corrections

--- React_advanced/togglelight/togglelight/src/ThemeContext.js
+++ React_advanced/togglelight/togglelight/src/ThemeContext.js
@@ -1,14 +1,20 @@
-import { createContext } from 'react';
+import { createContext, useContext, useState } from 'react';
 
-const ThemeContext = createContext();
+const ThemeContext = createContext(undefined);
 
 export const ThemeProvider = ({ children }) => {
 	const [theme, setTheme] = useState('light');
 
-	const toggleTheme = () => {
-		setTheme(theme === 'light' ? 'dark' : 'light');
-	};
-
-	return <ThemeContext.Provider value={{ theme: 'light' }}>{children}</ThemeContext.Provider>;
+	return (
+		<ThemeContext.Provider
+			value={{
+				theme,
+				toggleTheme: () => setTheme(theme === 'light' ? 'dark' : 'light'),
+			}}
+		>
+			{children}
+		</ThemeContext.Provider>
+	);
 };
-export const useTheme = () => ({ theme: 'light' });
+
+export const useTheme = () => useContext(ThemeContext);
