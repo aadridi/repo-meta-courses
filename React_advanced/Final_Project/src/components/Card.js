be1332dd05340541437a8f678e48b41c59961913
aadridi <aadridi@gmail.com>
2024-06-24 12:57:32 +0200

    Final Project Meta Front-End

--- React_advanced/Final_Project/src/components/Card.js
+++ React_advanced/Final_Project/src/components/Card.js
@@ -1,13 +1,30 @@
-import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import React from "react";
+import { Box, Heading, HStack, Image, Text, VStack } from '@chakra-ui/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import React from 'react';
 
 const Card = ({ title, description, imageSrc }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+	// Implement the UI for the Card component according to the instructions.
+	// You should be able to implement the component with the elements imported above.
+	// Feel free to import other UI components from Chakra UI if you wish to.
+	return (
+		<>
+			<Box>
+				<Box bg='white' borderRadius='10px' spacing={10}>
+					<Image src={imageSrc} borderRadius='10px' />
+					<Heading color='black' as='h3' size='md' p='4'>
+						{title}
+					</Heading>
+					<Text color='grey' p='4'>
+						{description}
+					</Text>
+					<Text color='black' p='4'>
+						See more <FontAwesomeIcon icon={faArrowRight} size='1x' />
+					</Text>
+				</Box>
+			</Box>
+		</>
+	);
 };
 
 export default Card;
--- React_advanced/Final_Project/src/components/ContactMeSection.js
+++ React_advanced/Final_Project/src/components/ContactMeSection.js
@@ -1,91 +1,84 @@
-import React, {useEffect} from "react";
-import { useFormik } from "formik";
-import {
-  Box,
-  Button,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Heading,
-  Input,
-  Select,
-  Textarea,
-  VStack,
-} from "@chakra-ui/react";
+import React, { useEffect } from 'react';
+import { useFormik } from 'formik';
+import { Box, Button, FormControl, FormErrorMessage, FormLabel, Heading, Input, Select, Textarea, VStack, FormHelperText } from '@chakra-ui/react';
 import * as Yup from 'yup';
-import FullScreenSection from "./FullScreenSection";
-import useSubmit from "../hooks/useSubmit";
-import {useAlertContext} from "../context/alertContext";
+import FullScreenSection from './FullScreenSection';
+import useSubmit from '../hooks/useSubmit';
+import { useAlertContext } from '../context/alertContext';
+import { Component } from 'react';
 
 const LandingSection = () => {
-  const {isLoading, response, submit} = useSubmit();
-  const { onOpen } = useAlertContext();
+	const { isLoading, response, submit } = useSubmit();
+	const { onOpen, onClose, isOpen, type, message } = useAlertContext();
 
-  const formik = useFormik({
-    initialValues: {},
-    onSubmit: (values) => {},
-    validationSchema: Yup.object({}),
-  });
+	const formik = useFormik({
+		initialValues: {
+			firstName: '',
+			email: '',
+			type: '',
+			comment: '',
+		},
+		onSubmit: async (values, actions) => {
+			const res = await submit('/submit', values);
+			if (res?.type === 'success') {
+				actions.resetForm();
+			}
+		},
+		validationSchema: Yup.object({
+			firstName: Yup.string().required('Required'),
+			email: Yup.string().email('Invalid Email Address').required('Required'),
+			type: Yup.string().optional(),
+			comment: Yup.string().min(25, 'Must be at least 25 characters').required('Required'),
+		}),
+	});
 
-  return (
-    <FullScreenSection
-      isDarkBackground
-      backgroundColor="#512DA8"
-      py={16}
-      spacing={8}
-    >
-      <VStack w="1024px" p={32} alignItems="flex-start">
-        <Heading as="h1" id="contactme-section">
-          Contact me
-        </Heading>
-        <Box p={6} rounded="md" w="100%">
-          <form>
-            <VStack spacing={4}>
-              <FormControl isInvalid={false}>
-                <FormLabel htmlFor="firstName">Name</FormLabel>
-                <Input
-                  id="firstName"
-                  name="firstName"
-                />
-                <FormErrorMessage></FormErrorMessage>
-              </FormControl>
-              <FormControl isInvalid={false}>
-                <FormLabel htmlFor="email">Email Address</FormLabel>
-                <Input
-                  id="email"
-                  name="email"
-                  type="email"
-                />
-                <FormErrorMessage></FormErrorMessage>
-              </FormControl>
-              <FormControl>
-                <FormLabel htmlFor="type">Type of enquiry</FormLabel>
-                <Select id="type" name="type">
-                  <option value="hireMe">Freelance project proposal</option>
-                  <option value="openSource">
-                    Open source consultancy session
-                  </option>
-                  <option value="other">Other</option>
-                </Select>
-              </FormControl>
-              <FormControl isInvalid={false}>
-                <FormLabel htmlFor="comment">Your message</FormLabel>
-                <Textarea
-                  id="comment"
-                  name="comment"
-                  height={250}
-                />
-                <FormErrorMessage></FormErrorMessage>
-              </FormControl>
-              <Button type="submit" colorScheme="purple" width="full">
-                Submit
-              </Button>
-            </VStack>
-          </form>
-        </Box>
-      </VStack>
-    </FullScreenSection>
-  );
+	useEffect(() => {
+		if (response) {
+			onOpen(response.type, response.message);
+		}
+	}, [response, onOpen]);
+
+	return (
+		<FullScreenSection isDarkBackground backgroundColor='#512DA8' py={16} spacing={8}>
+			<VStack w='1024px' p={32} alignItems='flex-start'>
+				<Heading as='h1' id='contactme-section'>
+					Contact me
+				</Heading>
+				<Box p={6} rounded='md' w='100%'>
+					<form onSubmit={formik.handleSubmit}>
+						<VStack spacing={4}>
+							<FormControl isInvalid={formik.touched.firstName && !!formik.errors.firstName}>
+								<FormLabel htmlFor='firstName'>Name</FormLabel>
+								<Input id='firstName' name='firstName' value={formik.values.firstName} onChange={formik.handleChange} onBlur={formik.handleBlur} placeholder='Enter your name here' />
+								<FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
+							</FormControl>
+							<FormControl isInvalid={formik.touched.email && !!formik.errors.email}>
+								<FormLabel htmlFor='email'>Email Address</FormLabel>
+								<Input id='email' name='email' type='email' value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} placeholder='Enter your email address here' />
+								<FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+							</FormControl>
+							<FormControl>
+								<FormLabel htmlFor='type'>Type of enquiry</FormLabel>
+								<Select id='type' name='type' value={formik.values.type} onChange={formik.handleChange} onBlur={formik.handleBlur}>
+									<option value='hireMe'>Freelance project proposal</option>
+									<option value='openSource'>Open source consultancy session</option>
+									<option value='other'>Other</option>
+								</Select>
+							</FormControl>
+							<FormControl isInvalid={formik.touched.comment && !!formik.errors.comment}>
+								<FormLabel htmlFor='comment'>Your message</FormLabel>
+								<Textarea id='comment' name='comment' height={250} value={formik.values.comment} onChange={formik.handleChange} onBlur={formik.handleBlur} placeholder='Enter your message here' />
+								<FormErrorMessage>{formik.errors.comment}</FormErrorMessage>
+							</FormControl>
+							<Button type='submit' isLoading={isLoading || formik.isSubmitting} colorScheme='purple' width='full'>
+								Submit
+							</Button>
+						</VStack>
+					</form>
+				</Box>
+			</VStack>
+		</FullScreenSection>
+	);
 };
 
 export default LandingSection;
