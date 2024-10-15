54b077f74136db910afe410461e53eab11953592
aadridi <aadridi@gmail.com>
2024-10-15 18:45:02 +0200

    Adding Bootstrap in the dependencies

--- Meta_FE_capstone/FE_capstone/src/components/Nav.jsx
+++ Meta_FE_capstone/FE_capstone/src/components/Nav.jsx
@@ -3,40 +3,44 @@ import le_logo from '../assets/le_logo.svg';
 function Nav() {
 	return (
 		<>
-			<nav>
-				<img src={le_logo} className='logo react' alt='Little Lemon Logo' />
-				<ul>
-					<li>
-						<a href='' target='_blank'>
-							Home
-						</a>
-					</li>
-					<li>
-						<a href='' target='_blank'>
-							About
-						</a>
-					</li>
-					<li>
-						<a href='' target='_blank'>
-							Menu
-						</a>
-					</li>
-					<li>
-						<a href='' target='_blank'>
-							Reservations
-						</a>
-					</li>
-					<li>
-						<a href='' target='_blank'>
-							Order Online
-						</a>
-					</li>
-					<li>
-						<a href='' target='_blank'>
-							Login
-						</a>
-					</li>
-				</ul>
+			<nav className='container text-center'>
+				<div className='row'>
+					<div className='col-6'>
+						<img src={le_logo} className='logo' alt='Little Lemon Logo' />
+					</div>
+					<ul className='col-6'>
+						<li className='col'>
+							<a href='#' target='_blank'>
+								Home
+							</a>
+						</li>
+						<li className='col'>
+							<a href='#' target='_blank'>
+								About
+							</a>
+						</li>
+						<li className='col'>
+							<a href='#' target='_blank'>
+								Menu
+							</a>
+						</li>
+						<li className='col'>
+							<a href='#' target='_blank'>
+								Reservations
+							</a>
+						</li>
+						<li className='col'>
+							<a href='#' target='_blank'>
+								Order Online
+							</a>
+						</li>
+						<li className='col'>
+							<a href='#' target='_blank'>
+								Login
+							</a>
+						</li>
+					</ul>
+				</div>
 			</nav>
 		</>
 	);
