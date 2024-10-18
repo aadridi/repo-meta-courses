a56c0fe955689a30bbb54ff70bba82d2c8f67ac4
aadridi <aadridi@gmail.com>
2024-10-18 10:50:25 +0200

    Starting working on HTML & CSS regarding Nav & Main

--- Meta_FE_capstone/FE_capstone/src/components/Main.jsx
+++ Meta_FE_capstone/FE_capstone/src/components/Main.jsx
@@ -1,8 +1,16 @@
 function Main() {
 	return (
 		<>
-			<main>
-				<h1>Main</h1>
+			<main className='main-container'>
+				<div className='first-main-blank'>Main</div>
+				<div className='main-content'>
+					<h1>Little Lemon</h1>
+					<h2>Chicago</h2>
+					<p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+					<button>Reserve a Table</button>
+				</div>
+				<div className='main-picture'>Picture</div>
+				<div className='second-main-blank'>Blank</div>
 			</main>
 		</>
 	);
--- Meta_FE_capstone/FE_capstone/src/components/Nav.jsx
+++ Meta_FE_capstone/FE_capstone/src/components/Nav.jsx
@@ -3,44 +3,41 @@ import le_logo from '../assets/le_logo.svg';
 function Nav() {
 	return (
 		<>
-			<nav className='container text-center'>
-				<div className='row'>
-					<div className='col-6'>
-						<img src={le_logo} className='logo' alt='Little Lemon Logo' />
-					</div>
-					<ul className='col-6'>
-						<li className='col'>
-							<a href='#' target='_blank'>
-								Home
-							</a>
-						</li>
-						<li className='col'>
-							<a href='#' target='_blank'>
-								About
-							</a>
-						</li>
-						<li className='col'>
-							<a href='#' target='_blank'>
-								Menu
-							</a>
-						</li>
-						<li className='col'>
-							<a href='#' target='_blank'>
-								Reservations
-							</a>
-						</li>
-						<li className='col'>
-							<a href='#' target='_blank'>
-								Order Online
-							</a>
-						</li>
-						<li className='col'>
-							<a href='#' target='_blank'>
-								Login
-							</a>
-						</li>
-					</ul>
-				</div>
+			<nav className='nav-container'>
+				<div className='nav-blank'>Blank</div>
+				<img src={le_logo} className='logo' alt='Little Lemon Logo' />
+				<ul className='nav-items-container'>
+					<li className='nav-links'>
+						<a href='#' target='_blank'>
+							Home
+						</a>
+					</li>
+					<li className='nav-links'>
+						<a href='#' target='_blank'>
+							About
+						</a>
+					</li>
+					<li className='nav-links'>
+						<a href='#' target='_blank'>
+							Menu
+						</a>
+					</li>
+					<li className='nav-links'>
+						<a href='#' target='_blank'>
+							Reservations
+						</a>
+					</li>
+					<li className='nav-links'>
+						<a href='#' target='_blank'>
+							Order Online
+						</a>
+					</li>
+					<li className='nav-links'>
+						<a href='#' target='_blank'>
+							Login
+						</a>
+					</li>
+				</ul>
 			</nav>
 		</>
 	);
