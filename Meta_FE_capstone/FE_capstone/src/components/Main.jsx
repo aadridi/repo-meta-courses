9e447c40b160276a3b55c2da8afbe6c5661196bf
aadridi <aadridi@gmail.com>
2024-12-17 23:07:12 +0100

    Finalizing Meta Front-End Capstone project, working on CSS to make the website responsive

--- Meta_FE_capstone/FE_capstone/src/components/Main.jsx
+++ Meta_FE_capstone/FE_capstone/src/components/Main.jsx
@@ -1,4 +1,5 @@
 import restauranfood from '../assets/restauranfood.jpg';
+import { Link } from 'react-router-dom';
 
 function Main() {
 	return (
@@ -8,7 +9,9 @@ function Main() {
 					<h1>Little Lemon</h1>
 					<h2>Chicago</h2>
 					<p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-					<button>Reserve a Table</button>
+					<Link to='/reservations'>
+						<button className='scroll-link'>Reserve a Table</button>
+					</Link>
 				</div>
 				<div className='main-picture'>
 					<img src={restauranfood} className='food-image' alt='Food Image' />
